d09cb80878c755d4400cd816b6d20ea061bf881a
tsomie <153620296+tsomie@users.noreply.github.com>
2023-12-27 20:29:00 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+      <!-- Include Tailwind CSS in your project -->
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        </head>
+        <body>
+          <!-- Create a header with the sticky class -->
+            <header class="sticky top-0 bg-green-400 shadow-md z-10">
+                <!-- Use flex and items-center to align the logo and the menu -->
+                    <div class="container mx-auto flex items-center justify-between py-4 px-6">
+                          <!-- Use an image tag for the logo and set its height with h-12 -->
+                                <img src="logo.png" alt="Logo" class="h-12">
+                                      <!-- Use a list for the menu and add some spacing with space-x-4 -->
+                                            <ul class="flex space-x-4">
+                                                    <li><a href="#" class="text-gray-700 hover:text-blue-500">Home</a></li>
+                                                            <li><a href="#" class="text-gray-700 hover:text-blue-500">About</a></li>
+                                                                    <li><a href="#" class="text-gray-700 hover:text-blue-500">Services</a></li>
+                                                                            <li><a href="#" class="text-gray-700 hover:text-blue-500">Contact</a></li>
+                                                                                  </ul>
+                                                                                      </div>
+                                                                                        </header>
+                                                                                        </html>
